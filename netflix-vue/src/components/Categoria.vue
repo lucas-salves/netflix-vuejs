38895f074a21b53debabec77d251340f92f800a5
lucas.salves@terra.com.br <lucas.salves@terra.com.br>
2020-03-07 22:21:21 -0300

    Categoria component creation

---
+++ netflix-vue/src/components/Categoria.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="slider">
+      <h3>Ação</h3>
+      <span @mouseover="scrollToLeft" @mouseout="clearScroll" class="handle handlePrev active">
+        <i class="fa fa-caret-left" aria-hidden="true"></i>
+      </span>
+
+      <div id="scroller" class="row">
+        <div class="row__inner">
+          <div class="gui-card">
+            <div class="gui-card__media">
+              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
+            </div>
+            <div class="gui-card__details">
+              <div class="gui-card__title">
+                Assassin’s Creed
+              </div>
+            </div>
+          </div>
+
+          <div class="gui-card">
+            <div class="gui-card__media">
+              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
+            </div>
+            <div class="gui-card__details">
+              <div class="gui-card__title">
+                Assassin’s Creed
+              </div>
+            </div>
+          </div>
+
+          <div class="gui-card">
+            <div class="gui-card__media">
+              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
+            </div>
+            <div class="gui-card__details">
+              <div class="gui-card__title">
+                Assassin’s Creed
+              </div>
+            </div>
+          </div>
+
+          <div class="gui-card">
+            <div class="gui-card__media">
+              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
+            </div>
+            <div class="gui-card__details">
+              <div class="gui-card__title">
+                Assassin’s Creed
+              </div>
+            </div>
+          </div>
+
+          <div class="gui-card">
+            <div class="gui-card__media">
+              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
+            </div>
+            <div class="gui-card__details">
+              <div class="gui-card__title">
+                Assassin’s Creed
+              </div>
+            </div>
+          </div>
+
+          <div class="gui-card">
+            <div class="gui-card__media">
+              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
+            </div>
+            <div class="gui-card__details">
+              <div class="gui-card__title">
+                Assassin’s Creed
+              </div>
+            </div>
+          </div>
+
+        </div> 
+      </div>
+
+      <span @mouseover="scrollToRight"  @mouseout="clearScroll" class="handle handleNext active">
+        <i class="fa fa-caret-right" aria-hidden="true"></i>
+      </span>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'categoria'
+}
+</script>
+
+<style >
+
+</style>
